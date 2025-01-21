e6c3dc9161a68b1bb3567ad332fe99908af14e8f
seraphh01 <sserafim.socaciu@gmail.com>
2025-01-21 18:54:53 +0200

    version

--- lib/pages/settings_page/settings_page_widget.dart
+++ lib/pages/settings_page/settings_page_widget.dart
@@ -695,7 +695,7 @@ class _SettingsPageWidgetState extends State<SettingsPageWidget> {
                                       ),
                                 ),
                                 Text(
-                                  'Versiunea curentă: 1.0.31',
+                                  'Versiunea curentă: 1.0.32',
                                   style: FlutterFlowTheme.of(context)
                                       .bodySmall
                                       .override(
