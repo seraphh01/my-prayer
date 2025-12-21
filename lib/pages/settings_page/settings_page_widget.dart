705c43bff2d62910ea145bffbd02e7bae8d027d0
Serafim Socaciu <sserafim.socaciu@gmail.com>
2025-12-21 14:35:36 +0200

    update icons

--- lib/pages/settings_page/settings_page_widget.dart
+++ lib/pages/settings_page/settings_page_widget.dart
@@ -707,7 +707,7 @@ class _SettingsPageWidgetState extends State<SettingsPageWidget> {
                                       ),
                                 ),
                                 Text(
-                                  'Versiunea curentă: 1.0.44',
+                                  'Versiunea curentă: 1.0.45',
                                   style: FlutterFlowTheme.of(context)
                                       .bodySmall
                                       .override(
